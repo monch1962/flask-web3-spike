e68367af8c8c0109172ed806462ba2c32e50fed2
David Mitchell <monch1962@gmail.com>
2022-05-15 13:28:50 +0000

    Started adding smart contracts

--- contracts/user.sol
+++ contracts/user.sol
@@ -1,11 +1,12 @@
-pragma solidity ^0.4.21;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 // import library file
-import “stringUtils.sol”;
+import "./stringUtils.sol" as stringUtils;
 
 contract userRecords {
     // enum type variable to store user gender
-    enum genderType { male, female };
+    enum genderType { male, female }
 
     // Actual user object which we will store
     struct user {
@@ -17,8 +18,8 @@ contract userRecords {
     user user_obj;
 
     //Internal function to conver genderType enum from string
-    function getGenderFromString(string gender) internal returns (genderType) {
-        if(StringUtils.equal(gender, “male”)) {
+    function getGenderFromString(string memory gender) internal returns (genderType) {
+        if(stringUtils.equal(gender, "male")) {
             return genderType.male;
         } else {
             return genderType.female;
@@ -26,24 +27,24 @@ contract userRecords {
     }
 
     //Internal function to convert genderType enum to string
-    function getGenderToString(genderType gender) internal returns (string) {
+    function getGenderToString(genderType gender) internal returns (string memory) {
         if(gender == genderType.male) {
-            return “male”;
+            return "male";
         } else {
-            return “female”;
+            return "female";
         }
     }
 
     // set user public function
     // This is similar to persisting object in db.
-    function setUser(string name, string gender) public {
+    function setUser(string memory name, string memory gender) public {
         genderType gender_type = getGenderFromString(gender);
         user_obj = user({name:name, gender: gender_type});
     }
     
     // get user public function
     // This is similar to getting object from db.
-    function getUser() public returns (string, string) {
+    function getUser() public returns (string memory, string memory) {
         return (user_obj.name, getGenderToString(user_obj.gender));
     }
 }
